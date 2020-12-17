076a5fd3c6a00d55d4101a6f3fbeefaab7be3f0a
D4 <d4@thegrid.io>
2020-12-17 04:25:18 -0800

    build tweaks, resizable demow window

--- demo/renderer.c
+++ demo/renderer.c
@@ -22,7 +22,9 @@ void r_init(void) {
   /* init SDL window */
   window = SDL_CreateWindow(
     NULL, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    width, height, SDL_WINDOW_OPENGL);
+    width, height, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE );
+  SDL_MaximizeWindow(window);
+  SDL_GetWindowSize(window, &width, &height);
   SDL_GL_CreateContext(window);
 
   /* init gl */
